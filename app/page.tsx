1155277ffd8a6fab9f0f9e69febac2c886ecb040
v0agent <it+v0agent@vercel.com>
2026-06-04 04:28:39 +0000

    feat: add BrandHeader component to Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,21 @@
 import { PlayerProvider } from "@/contexts/player-context"
 import { VinylPlayer } from "@/components/vinyl-player"
 import { SupportPrompt } from "@/components/support-prompt"
+import { BrandHeader } from "@/components/brand-header"
 
 export default function Home() {
   return (
-    <PlayerProvider>
-      <main className="w-full">
-        <VinylPlayer />
-        <SupportPrompt
-          instagramUrl="https://www.instagram.com/raya.1ity"
-          youtubeUrl="https://www.youtube.com/@raya.1ity"
-        />
-      </main>
-    </PlayerProvider>
+    <>
+      <BrandHeader />
+      <PlayerProvider>
+        <main className="w-full">
+          <VinylPlayer />
+          <SupportPrompt
+            instagramUrl="https://www.instagram.com/raya.1ity"
+            youtubeUrl="https://www.youtube.com/@raya.1ity"
+          />
+        </main>
+      </PlayerProvider>
+    </>
   )
 }
